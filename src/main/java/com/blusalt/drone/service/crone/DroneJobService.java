e2264727508051e59535fd052da874575886868a
olutayopeter <olutayopeter2014@gmail.com>
2023-07-05 03:07:05 -0700

    Cron service update

--- src/main/java/com/blusalt/drone/service/crone/DroneJobService.java
+++ src/main/java/com/blusalt/drone/service/crone/DroneJobService.java
@@ -14,99 +14,99 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Collections;
 import java.util.List;
 
-//@Configuration
-//@EnableScheduling
+@Configuration
+@EnableScheduling
 public class DroneJobService {
 
-//    @Autowired
-//    DroneRepository droneRepository;
-//
-//    @Autowired
-//    MedicationRepository medicationRepository;
-//
-//    @Scheduled(fixedDelayString = "${drone.delay}")
-//    public void run() {
-//        startCharging();
-//        checkDroneBattery();
-//        startReturning();
-//        endDelivery();
-//        startDelivery();
-//    }
-//
-//    private List<Drone> transitionState(State previous, State next) {
-//        List<Drone> drones = droneRepository.findAllByState(previous);
-//        if (drones.size() == 0) return Collections.emptyList();
-//        for (Drone drone : drones) {
-//            drone.setState(next);
-//        }
-//        return droneRepository.saveAll(drones);
-//    }
-//
-//    private void startDelivery() {
-//        transitionState(State.LOADED, State.DELIVERING);
-//    }
-//
-//    void endDelivery() {
-//
-//        List<Drone> drones = transitionState(State.DELIVERING, State.DELIVERED);
-//
-//
-//        if (drones.size() == 0) {
-//            return;
-//        }
-//
-//        medicationRepository.updateMedication(drones);
-//
-//
-//    }
-//
-//    private void startReturning() {
-//        transitionState(State.DELIVERED, State.RETURNING);
-//    }
-//
-//    private void checkDroneBattery() {
-//
-//        List<Drone> drones = droneRepository.findAllByState(State.RETURNING);
-//        if (drones.size() == 0) {
-//            return;
-//        }
-//        for (Drone drone : drones) {
-//            int batteryCapacity = drone.getBatteryCapacity() - 25;
-//            drone.setBatteryCapacity(batteryCapacity);
-//
-//            if (batteryCapacity < 25) {
-//                drone.setBatteryCapacity(0);
-//                drone.setState(State.IDLE);
-//            } else {
-//                drone.setState(State.LOADING);
-//            }
-//
-//        }
-//
-//        droneRepository.saveAll(drones);
-//    }
-//
-//    public void startCharging() {
-//
-//        List<Drone> drones = droneRepository.findAllByState(State.IDLE);
-//        if (drones.size() == 0) {
-//            return;
-//        }
-//        for (Drone drone : drones) {
-//            int batteryCapacity = drone.getBatteryCapacity() + 25;
-//            drone.setBatteryCapacity(batteryCapacity);
-//
-//            if (batteryCapacity >= 100) {
-//                drone.setBatteryCapacity(100);
-//                drone.setState(State.LOADING);
-//            } else {
-//                drone.setState(State.IDLE);
-//            }
-//
-//        }
-//
-//        droneRepository.saveAll(drones);
-//    }
+    @Autowired
+    DroneRepository droneRepository;
+
+    @Autowired
+    MedicationRepository medicationRepository;
+
+    @Scheduled(fixedDelayString = "${drone.delay}")
+    public void run() {
+        startCharging();
+        checkDroneBattery();
+        startReturning();
+        endDelivery();
+        startDelivery();
+    }
+
+    private List<Drone> transitionState(State previous, State next) {
+        List<Drone> drones = droneRepository.findAllByState(previous.toString());
+        if (drones.size() == 0) return Collections.emptyList();
+        for (Drone drone : drones) {
+            drone.setState(next);
+        }
+        return droneRepository.saveAll(drones);
+    }
+
+    private void startDelivery() {
+        transitionState(State.LOADED, State.DELIVERING);
+    }
+
+    void endDelivery() {
+
+        List<Drone> drones = transitionState(State.DELIVERING, State.DELIVERED);
+
+
+        if (drones.size() == 0) {
+            return;
+        }
+
+        medicationRepository.updateMedication(drones);
+
+
+    }
+
+    private void startReturning() {
+        transitionState(State.DELIVERED, State.RETURNING);
+    }
+
+    private void checkDroneBattery() {
+
+        List<Drone> drones = droneRepository.findAllByState(State.RETURNING.toString());
+        if (drones.size() == 0) {
+            return;
+        }
+        for (Drone drone : drones) {
+            int batteryCapacity = drone.getBatteryCapacity() - 25;
+            drone.setBatteryCapacity(batteryCapacity);
+
+            if (batteryCapacity < 25) {
+                drone.setBatteryCapacity(0);
+                drone.setState(State.IDLE);
+            } else {
+                drone.setState(State.LOADING);
+            }
+
+        }
+
+        droneRepository.saveAll(drones);
+    }
+
+    public void startCharging() {
+
+        List<Drone> drones = droneRepository.findAllByState(State.IDLE.toString());
+        if (drones.size() == 0) {
+            return;
+        }
+        for (Drone drone : drones) {
+            int batteryCapacity = drone.getBatteryCapacity() + 25;
+            drone.setBatteryCapacity(batteryCapacity);
+
+            if (batteryCapacity >= 100) {
+                drone.setBatteryCapacity(100);
+                drone.setState(State.LOADING);
+            } else {
+                drone.setState(State.IDLE);
+            }
+
+        }
+
+        droneRepository.saveAll(drones);
+    }
 
 
 }
